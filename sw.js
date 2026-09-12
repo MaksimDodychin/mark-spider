a0f772ceaa0ebd0264168c1e4154c51031012b58
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 00:15:59 +0300

    👥 Онлайн: комната по коду, профиль с аватаркой и ником, 2D-комната ожидания, чат фразами, голос

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = '32bda1fbf2';
+const VERSION = '90e1d8b5a3';
 const PREFIX = "spider-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
