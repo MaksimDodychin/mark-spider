ace07e30914c29d2f1a260c228579ca1ce1c265c
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 10:05:42 +0300

    👥 Комната открывается сразу на виду: 2D-человечки, «жди игроков», гардероб со шляпами, приглашение ссылкой

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = '37998ef2a0';
+const VERSION = '5146e18802';
 const PREFIX = "spider-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
